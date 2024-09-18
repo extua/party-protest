89165d24efc8551e9816067881fc99e1fa5d2683
Pierre Marshall <pierre.marshall@gmail.com>
2024-09-18 16:17:06 +0100

    add basic templating example to work on

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[macro_use] extern crate rocket;
 use rocket::fs::{FileServer, relative};
+use rocket_dyn_templates::Template;
+
+
+mod tera;
 
 #[get("/world")]
 fn world() -> &'static str {
@@ -9,6 +13,11 @@ fn world() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/hello", routes![world])
+        .mount("/tera", routes![tera::index, tera::hello, tera::about])
+        .register("/tera", catchers![tera::not_found])
+        .attach(Template::custom(|engines| {
+            tera::customize(&mut engines.tera);
+        }))
     .mount("/assets", FileServer::from(relative!("/assets")))
 }
 
