7caecf853c5810e48908487cc8b64028972f85d1
Pierre Marshall <pierre.marshall@gmail.com>
2024-09-26 22:23:48 +0100

    blank main.rs for now

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-#[macro_use] extern crate rocket;
-use rocket::fs::{FileServer, relative};
-use rocket_dyn_templates::Template;
+// #[macro_use] extern crate rocket;
+// use rocket::fs::{FileServer, relative};
+// use rocket_dyn_templates::Template;
 
 
-mod tera;
+// mod tera;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, world!"
-}
+// #[get("/world")]
+// fn world() -> &'static str {
+//     "Hello, world!"
+// }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![world])
-        .mount("/tera", routes![tera::index, tera::hello, tera::about])
-        .register("/tera", catchers![tera::not_found])
-        .attach(Template::custom(|engines| {
-            tera::customize(&mut engines.tera);
-        }))
-    .mount("/assets", FileServer::from(relative!("/assets")))
-}
+// #[launch]
+// fn rocket() -> _ {
+//     rocket::build().mount("/hello", routes![world])
+//         .mount("/tera", routes![tera::index, tera::hello, tera::about])
+//         .register("/tera", catchers![tera::not_found])
+//         .attach(Template::custom(|engines| {
+//             tera::customize(&mut engines.tera);
+//         }))
+//     .mount("/assets", FileServer::from(relative!("/assets")))
+// }
 
