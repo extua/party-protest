da154caed2bb430fbcc0eb0fbd651f9c7d9fb307
Pierre Marshall <pierre.marshall@gmail.com>
2024-10-01 11:35:13 +0100

    remove original src directory and clear cargo.toml
    this clarifies things for cargo to run

---
+++ migrations/2024-09-19-141042_create_events/up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE events (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR NOT NULL,
+  description TEXT NOT NULL,
+  start_time INTEGER NOT NULL,
+  end_time INTEGER,
+  location TEXT
+)
