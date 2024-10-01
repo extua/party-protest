36469e97bf496159b80bceeb42de8c98dad3e5e3
Pierre Marshall <pierre.marshall@gmail.com>
2024-10-01 12:51:56 +0100

    quote column names in SQL

--- infrastructure/migrations/2024-09-19-141042_create_events/up.sql
+++ infrastructure/migrations/2024-09-19-141042_create_events/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE events (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR NOT NULL,
-  description TEXT NOT NULL,
+  "name" VARCHAR NOT NULL,
+  "description" TEXT NOT NULL,
   start_time INTEGER NOT NULL,
   end_time INTEGER,
-  location TEXT
+  "location" TEXT
 )
